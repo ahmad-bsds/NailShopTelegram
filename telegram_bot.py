d54575ecc6433a216e2ba5a5d2db0c48df27d714
MUHAMMAD AHMAD <ahmad.bsds@gmail.com>
2024-12-19 15:09:57 +0500

    Started.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,29 +2,30 @@ from typing import Final
 from utils import load_env_variable, inference, get_logger
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-from quart import Quart, request
-import logging
+from keep_alive import keep_alive
+
+
+keep_alive()
 
 logger = get_logger(__name__)
 
-# Load environment variables
 TOKEN: Final = load_env_variable("TELEGRAM_TOKEN")
 BOT_USERNAME: Final = load_env_variable("TELEGRAM_USERNAME")
 
-# Telegram Bot Application
-application = Application.builder().token(TOKEN).build()
 
-
-# Define commands
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hi, I'm Nail Shop Service assistant! How can I help you?")
+    await update.message.reply_text(f"Hi, I'm Nail Shop Service assistant! How can I help you?")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        "Welcome! I'm here to help you shine. Need to book an appointment, check our services, "
-        "or get quick answers about your nail care? I've got you covered.\n"
-        "Your beauty, our passion! ✨"
+        """
+        Welcome! I'm here to help you shine. Need to book an appointment, check our services,\
+        or get quick answers about your nail care? I've got you covered.\
+        From gel polish to acrylics, manicures to pedicures,\
+        just ask and I'll make sure you get the perfect nail experience. Your beauty, our passion! ✨
+        
+        """
     )
 
 
@@ -40,20 +41,28 @@ def hande_response(prompt):
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Check if someone calling bot in the group then give it response.
+    :param update:
+    :param context:
+    :return: string text
+    """
     message_type = update.message.chat.type
     user_message = update.message.text
 
     logger.info(f"User ({update.message.chat.id}) in {message_type}: {user_message}.")
 
-    if message_type == "group" and BOT_USERNAME in user_message:
-        prompt: str = user_message.replace(f"{BOT_USERNAME} ", "").strip()
-        response: str = hande_response(prompt)
-    elif message_type != "group":
-        response = hande_response(user_message)
+    if message_type == "group":
+        if BOT_USERNAME in user_message:
+            prompt: str = user_message.replace(f"{BOT_USERNAME} ", "").strip()
+            response: str = hande_response(prompt)
+        else:
+            return
     else:
-        return
+        response = hande_response(user_message)
 
     logger.info(f"Response: {response}")
+
     await update.message.reply_text(response)
 
 
@@ -62,43 +71,25 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.error(f"Update {update} caused error {context.error}")
 
 
-# Add handlers
-application.add_handler(CommandHandler("start", start))
-application.add_handler(CommandHandler("help", help_command))
-application.add_handler(MessageHandler(filters.TEXT, handle_message))
-application.add_error_handler(error)
-
-# Quart app for webhook
-app = Quart(__name__)
-
-
-@app.route("/", methods=["GET"])
-async def index():
-    return "Hello, World!"
-
-
-@app.route("/webhook", methods=["POST"])
-async def webhook():
-    """Handle incoming webhook requests from Telegram."""
-    data = await request.get_json(force=True)
-    update = Update.de_json(data, application.bot)
-    application.update_queue.put(update)
-    return "Webhook Alive"
-
-
-# Main method to run the app
 if __name__ == "__main__":
     logger.info("Starting bot...")
+    application = Application.builder().token(TOKEN).build()
 
-    # Set webhook URL
-    WEBHOOK_URL = f"https://nailshoptelegram.onrender.com/webhook"
+    # Commands
+    start_handler = CommandHandler("start", start)
+    help_handler = CommandHandler("help", help_command)
 
-    try:
-        logger.info(f"Setting webhook to {WEBHOOK_URL}")
-        application.bot.set_webhook(WEBHOOK_URL)
-        logger.info("Webhook set successfully!")
-    except Exception as e:
-        logger.error(f"Failed to set webhook: {e}")
+    # Messages
+    message_handler = MessageHandler(filters.TEXT, handle_message)
 
-    # Run Quart app (ASGI server)
-    app.run(host="0.0.0.0", port=9090)
+    # Handlers
+    application.add_handler(start_handler)
+    application.add_handler(help_handler)
+    application.add_handler(message_handler)
+
+    # Log Errors
+    application.add_error_handler(error)
+
+    # Run
+    logger.info("Pooling...")
+    application.run_polling(poll_interval=5)
